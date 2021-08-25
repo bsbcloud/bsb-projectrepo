9dd1f45d591aad6db677d5f9378b0e7223fac46d
bsbcloud <bsbdevops@gmail.com>
2021-08-25 11:46:44 +0530

    feature101 changes by dev1

--- file1.java
+++ file1.java
@@ -9,6 +9,14 @@ int remainder = number % 10;
 reverse = reverse * 10 + remainder;  
 number = number/10;  
 }  
-System.out.println("The reverse of the given number is: " + reverse);  
-}  
+System.out.println("The reverse of the given number is: " + reverse);
+
+} 
+//feature101 changes by dev1
+// create a method
+  public static int square(int num) {
+
+    // return statement
+    return num * num;
+  }
 }  
